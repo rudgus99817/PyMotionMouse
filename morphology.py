ae82f23434765f2fb89d776e44dfb8dc130a5e10
rudgus99817 <rudgus99817@naver.com>
2016-09-08 22:05:29 +0900

    morphology.py modify, add mask.jpg

--- morphology.py
+++ morphology.py
@@ -1,18 +1,6 @@
 import cv2
 import numpy
 
-def Detect(original_image):
-    cv2.cvtColor(original_image,cv2.COLOR_BGR2HSV)
-    row = image.shape[0]
-    col = image.shape[1]
-    for i in range(row):
-        for j in range(col):
-            if(0<=image[i][j][0]<120 and 50<=image[i][j][1]<255 and 80<=image[i][j][2]<255):
-                image[i][j] = 255
-            else:
-                image[i][j] = 0
-    return image
-
 def Erode(image,num,kernel):
     for i in range(num):
         image = cv2.erode(image,kernel)
@@ -21,16 +9,21 @@ def Erode(image,num,kernel):
 if __name__ == "__main__":
     cv2.namedWindow("test")
     cv2.namedWindow("test2")
-    image = cv2.imread("b.jpg")
-    image = Detect(image)
-    kernel = numpy.ones((7,7),numpy.uint8)
-    test1 = Erode(image,1,numpy.ones((5,5),numpy.uint8))
-    test2 = Erode(image,1,kernel)
+    image = cv2.imread("mask.jpg")
+    kernel = numpy.ones((3,3),numpy.uint8)
+    
+    #시행 횟수에 따른 차이점
+    test1 = Erode(image,1,kernel)
+    test2 = Erode(image,2,kernel)
+    
+    #kernel 사이즈에 따른 차이점
+    #test1 = Erode(image,1,kernel)
+    #test2 = Erode(image,2,numpy.ones((5,5),numpy.uint8))
     while 1:
         if(cv2.waitKey(1) >= 27):
             break;
         cv2.imshow("test",test1)
-        #cv2.imshow("test2",test2)
+        cv2.imshow("test2",test2)
     cv2.destroyWindow("test")
-    #cv2.destroyWindow("test2")
+    cv2.destroyWindow("test2")
     
